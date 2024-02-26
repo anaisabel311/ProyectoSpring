e3ff8479ff06f85f553172ddd452064bf08c4f54
anaisabel311 <anaisabelarribas@hotmail.com>
2024-02-26 20:00:58 +0100

    26/02/2024

--- src/main/java/herenciaGenero/BeanPersonaHerencia.java
+++ src/main/java/herenciaGenero/BeanPersonaHerencia.java
@@ -1,21 +1,44 @@
 package herenciaGenero;
 
+import java.util.List;
+import java.util.Scanner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class BeanPersonaHerencia {
+	
+	
+	@Autowired
+	private BeanServicioPrensa servicioPrensa;
 	
 	private String nombre;
 	private String apellidos;
 	private String dni;
 	private String genero;
+	private String tipodeSuscripcion;
 	
-	public BeanPersonaHerencia(String nombre, String apellidos, String dni) {
+	public BeanPersonaHerencia(String nombre, String apellidos, String dni, String tipodeSuscripcion) {
 		super();
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 		this.dni = dni;
+		this.tipodeSuscripcion = tipodeSuscripcion;
 		
 	}
+
+	
+	
+	public BeanServicioPrensa getServicioPrensa() {
+		return servicioPrensa;
+	}
+
+
+
+	public void setServicioPrensa(BeanServicioPrensa servicioPrensa) {
+		this.servicioPrensa = servicioPrensa;
+	}
+
+
 
 	public String getNombre() {
 		return nombre;
@@ -49,12 +72,46 @@ public class BeanPersonaHerencia {
 		this.genero = genero;
 	}
 
+	public String getTipodeSuscripcion() {
+		return tipodeSuscripcion;
+	}
+
+	public void setTipodeSuscripcion(String genero) {
+		this.tipodeSuscripcion = tipodeSuscripcion;
+	}
+	
 	@Override
 	public String toString() {
-		return "PersonaHerenciaSpring [nombre=" + nombre + ", apellidos=" + apellidos + ", dni=" + dni + ", genero="
-				+ genero + "]";
+		return "BeanPersonaHerencia [nombre=" + nombre + ", apellidos=" + apellidos + ", dni=" + dni + ", genero="
+				+ genero + ", tipodeSuscripcion=" + tipodeSuscripcion + "]";
 	}
 	
 	
-
+	public void solicitarLectura() {
+		List<String> periodicos = servicioPrensa.mostrarPrensa(tipodeSuscripcion);
+		System.out.println("Estos son los periodicos que puedes leer: ");
+			
+			for (String periodico : periodicos) {
+				System.out.println(periodico);
+			}
+		
+		
+	}
+	
+	
+	public String leer (String periodico) {
+		
+		System.out.println("Selecciona un periodico");
+		Scanner scan=new Scanner(System.in);
+		String periodicoSeleccionado=scan.nextLine();
+		
+		return periodicoSeleccionado;
+	
+	}
+	
+	public void iniciarLectura() {
+		
+		solicitarLectura();
+		//leer(solicitarLectura());
+	}
 }
--- src/main/java/herenciaGenero/GeneroMain.java
+++ src/main/java/herenciaGenero/GeneroMain.java
@@ -6,14 +6,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class GeneroMain {
 
 	public static void main(String[] args) {
+		
 		ApplicationContext context=new ClassPathXmlApplicationContext("HerenciaGenero.xml");
 		
 		
-		BeanPersonaHerencia p1 =(BeanPersonaHerencia)context.getBean("persona1");
-		System.out.println(p1);
+		//BeanPersonaHerencia p1 =(BeanPersonaHerencia)context.getBean("persona1");
+		//System.out.println(p1);
 		BeanPersonaHerencia p2 =(BeanPersonaHerencia)context.getBean("persona2");
 		System.out.println(p2);
+		BeanPersonaHerencia p3 =(BeanPersonaHerencia)context.getBean("persona3");
+		System.out.println(p3);
+		
+		
+		p2.iniciarLectura();
+		
 		
 	}
 
+	
+	
 }
